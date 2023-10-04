1ee2c92db669d32a7b2fb8d45c41afdc5edc6008
gbkabel <112282802+gbkabel@users.noreply.github.com>
2023-10-03 18:10:05 -0700

    Adding strategy function

--- code/Blackjack.h
+++ code/Blackjack.h
@@ -9,7 +9,6 @@ class Blackjack
 public:
     void blackjackGame(); // Game engine
 
-
 private:
     void dealCards(vector<int>& hand);    // Deals Cards to both player and Dealer
     static int randomCard();             // Gets a random card that is added to hand
@@ -17,6 +16,7 @@ private:
     static char toUpperCase(char choice);           // Capitalizes characters to cross examine
     void playerHit(vector<int>& hand, int& total);  // Hit function for the player
     void playerStand(vector<int>& hand, int& total); // Stand function for the player/Dealers hit function
+    void checkStrategy(const int playerHand, const int dealerHand); // Checks Blackjack Basic Strategy
     static bool isGameOver(int);        // Checks if the game is over
     static char anotherGame();          // Checks if the user wants to play again
 
