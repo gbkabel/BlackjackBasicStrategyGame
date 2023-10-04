a4951af1b3ba8592aa0d084fb8362f951c8ad19e
gbkabel <112282802+gbkabel@users.noreply.github.com>
2023-10-03 17:30:22 -0700

    Added comments and playerHit and playerStand functions

--- code/Blackjack.h
+++ code/Blackjack.h
@@ -6,20 +6,20 @@ using namespace std;
 
 class Blackjack
 {
-  public:
-    void blackjackGame();
+public:
+    void blackjackGame(); // Game engine
 
 
-  private:
-    static int randomCard();
-    static int calcTotal(const vector<int>& hand);
-    static char toUpperCase(char choice);
-    static bool isGameOver(int);
-    static char anotherGame();
+private:
+    void dealCards(vector<int>& hand);    // Deals Cards to both player and Dealer
+    static int randomCard();             // Gets a random card that is added to hand
+    static int calcTotal(const vector<int>& hand);  // Totals a hand up
+    static char toUpperCase(char choice);           // Capitalizes characters to cross examine
+    void playerHit(vector<int>& hand, int& total);  // Hit function for the player
+    void playerStand(vector<int>& hand, int& total); // Stand function for the player/Dealers hit function
+    static bool isGameOver(int);        // Checks if the game is over
+    static char anotherGame();          // Checks if the user wants to play again
 
-    // Hand dealt to the player
-    vector<int> playerHand;
-
-    // Hand dealt to the dealer
-    vector<int> dealerHand;
+    vector<int> playerHand;        // Hand dealt to the player
+    vector<int> dealerHand;        // Hand dealt to the dealer
 };
